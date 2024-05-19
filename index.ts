22338cd42872c0c46ca78ba89a1d5f33ea9e33a0
newbee1939 <hideto3970@gmail.com>
2024-05-19 21:41:38 +0900

    updae

--- index.ts
+++ index.ts
@@ -3,34 +3,34 @@ import axios from "axios";
 const ARTICLE_LIMIT = 5;
 
 // Zennの記事を取得
-const zennTitle = '【Zennのトレンド記事】\n'
+const zennTitle = '【Zennのトレンド記事】\n\n'
 const zennArticles = await axios.get("https://zenn.dev/api/articles/");
 const zennArticlesList = zennArticles.data.articles.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<https://zenn.dev${current.path}|${current.title}>\n` 
 }, zennTitle);
 
 // Publickeyの新着記事を取得
-const publickeyTitle = '【publickeyの新着記事】\n'
+const publickeyTitle = '【publickeyの新着記事】\n\n'
 const publickeyArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml') 
 const publickeyArticlesList = publickeyArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, publickeyTitle);
 
 // Qiitaのトレンド記事を取得
-const qiitaTitle = '【Qiitaのトレンド記事】\n'
+const qiitaTitle = '【Qiitaのトレンド記事】\n\n'
 const qiitaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://qiita.com/popular-items/feed') 
 const qiitaArticlesList = qiitaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, qiitaTitle);
 
 // はてなブックマークテクノロジーのトレンド記事を取得
-const hatenaTitle = '【はてなブックマーク(テクノロジー)のトレンド記事】\n'
+const hatenaTitle = '【はてなブックマーク(テクノロジー)のトレンド記事】\n\n'
 const hatenaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://b.hatena.ne.jp/hotentry/it.rss') 
 const hatenaArticlesList = hatenaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, hatenaTitle);
 
-const articles = ':robot_face:*本日の技術記事*' + "```" + `${hatenaArticlesList}\n${publickeyArticlesList}\n${zennArticlesList}\n${qiitaArticlesList}` + "```";
+const articles = ':robot_face:*本日の技術記事*' + "```" + `${hatenaArticlesList}\n\n${publickeyArticlesList}\n\n${zennArticlesList}\n\n${qiitaArticlesList}` + "```";
 
 const postData = new URLSearchParams();
 
