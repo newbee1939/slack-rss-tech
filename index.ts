045152a976b30a6a285b5e491245a52fe52773c3
newbee1939 <hideto3970@gmail.com>
2024-05-19 19:55:06 +0900

    定数か

--- index.ts
+++ index.ts
@@ -1,30 +1,32 @@
 import axios from "axios";
 
+const ARTICLE_LIMIT = 10;
+
 // Zennの記事を取得
 const zennTitle = '【Zennのトレンド記事】\n'
 const zennArticles = await axios.get("https://zenn.dev/api/articles/");
-const zennArticlesList = zennArticles.data.articles.slice(0, 5).reduce((prev: any, current: any, index: any) => {
+const zennArticlesList = zennArticles.data.articles.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<https://zenn.dev${current.path}|${current.title}>\n` 
 }, zennTitle);
 
 // Publickeyの新着記事を取得
 const publickeyTitle = '【publickeyの新着記事】\n'
 const publickeyArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml') 
-const publickeyArticlesList = publickeyArticles.data.items.slice(0, 5).reduce((prev: any, current: any, index: any) => {
+const publickeyArticlesList = publickeyArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, publickeyTitle);
 
 // Qiitaのトレンド記事を取得
 const qiitaTitle = '【Qiitaのトレンド記事】\n'
 const qiitaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://qiita.com/popular-items/feed') 
-const qiitaArticlesList = qiitaArticles.data.items.slice(0, 5).reduce((prev: any, current: any, index: any) => {
+const qiitaArticlesList = qiitaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, qiitaTitle);
 
 // はてなブックマークテクノロジーのトレンド記事を取得
 const hatenaTitle = '【はてなブックマーク(テクノロジー)のトレンド記事】\n'
 const hatenaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://b.hatena.ne.jp/hotentry/it.rss') 
-const hatenaArticlesList = hatenaArticles.data.items.slice(0, 5).reduce((prev: any, current: any, index: any) => {
+const hatenaArticlesList = hatenaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, hatenaTitle);
 
