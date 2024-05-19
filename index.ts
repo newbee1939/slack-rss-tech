6998592744b2dce1938e1d032958c691ee421c2d
newbee1939 <hideto3970@gmail.com>
2024-05-19 19:28:56 +0900

    update

--- index.ts
+++ index.ts
@@ -11,14 +11,14 @@ const zennArticlesList = zennArticles.data.articles.slice(0, 5).reduce((prev: an
 const publickeyTitle = '【publickeyの新着記事】\n'
 const publickeyArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml') 
 const publickeyArticlesList = publickeyArticles.data.items.slice(0, 5).reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<https://zenn.dev${current.link}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, publickeyTitle);
 
 // Qiitaのトレンド記事を取得
 const qiitaTitle = '【Qiitaのトレンド記事】\n'
 const qiitaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://qiita.com/popular-items/feed') 
 const qiitaArticlesList = qiitaArticles.data.items.slice(0, 5).reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<https://zenn.dev${current.link}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, qiitaTitle);
 
 const articles = ':robot_face:*今日の技術記事*' + "```" + `${zennArticlesList}\n${publickeyArticlesList}\n${qiitaArticlesList}` + "```";
