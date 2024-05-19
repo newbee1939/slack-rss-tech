d8942a7367c96380a5fcbc3ac97567267a120597
newbee1939 <hideto3970@gmail.com>
2024-05-19 19:16:16 +0900

    updae

--- index.ts
+++ index.ts
@@ -5,16 +5,16 @@ const zennTitle = '【Zennのトレンド】\n'
 const zennArticles = await axios.get("https://zenn.dev/api/articles/");
 const zennArticlesList = zennArticles.data.articles.slice(0, 5).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<https://zenn.dev${current.path}|${current.title}>\n` 
-}, '```' + zennTitle) + '```';
+}, zennTitle);
 
 // Publickeyの記事を取得
-const publickeyTitle = '【Zennのトレンド】\n'
+const publickeyTitle = '【publickeyの新着記事】\n'
 const publickeyArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml') 
 const publickeyArticlesList = publickeyArticles.data.items.slice(0, 5).reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<https://zenn.dev${current.link}|${current.title}>\n` 
-}, '```' + publickeyTitle) + '```';
+}, publickeyTitle);
 
-const articles = `${zennArticlesList}\n${publickeyArticlesList}`;
+const articles = ':robot_face:*今日の技術記事*' + "```" + `${zennArticlesList}\n${publickeyArticlesList}` + "```";
 
 const postData = new URLSearchParams();
 
