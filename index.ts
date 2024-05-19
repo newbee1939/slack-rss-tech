616c7788289382ba6045fcdca9f49701e0c0c095
newbee1939 <hideto3970@gmail.com>
2024-05-19 14:06:37 +0900

    投稿

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+const { WebClient } = require('@slack/web-api');
+
+(async () => {
+  // OAuth トークン
+  const token  = 'xoxb-6600661338418-7142231146308-WOzQWYz9kdW8Y0gl2DgbhWs9';
+  // #チャンネル名 of @ユーザー名
+  const channel = '#エンジニアリング';
+  // メッセージ
+  const text = '*Hello World*';
+
+  const client = new WebClient(token);
+  const response = await client.chat.postMessage({ channel, text });
+
+  // 投稿に成功すると `ok` フィールドに `true` が入る。
+  console.log(response.ok);
+  // => true
+})();
