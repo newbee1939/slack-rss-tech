97016b6b6ab1dfadf346b2f5398503f11555a029
newbee1939 <hideto3970@gmail.com>
2024-06-03 08:19:55 +0900

    update

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as fs from "node:fs/promises";
 
 const ARTICLE_LIMIT = 5; 
 
@@ -11,10 +12,27 @@ const zennArticlesList = zennArticles.data.articles.slice(0, ARTICLE_LIMIT).redu
 
 // Publickeyの新着記事を取得
 const publickeyTitle = '【<https://www.publickey1.jp/|Publickey>の新着記事】\n'
-const publickeyArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml') 
-const publickeyArticlesList = publickeyArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
+const publickeyArticles = (await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.publickey1.jp/atom.xml')).data.items 
+
+// Publickeyの前回の記事リンクリストを取得
+const prevPublickeyArticleLinks = await fs.readFile("tech-rss/publickey.json", null);
+// JSON.parse(data)
+
+const requiredPublickeyArticleList = publickeyArticles.slice(0, ARTICLE_LIMIT).filter((item: any) => {
+  // NOTE: "新着"記事なので、前回表示した記事は除外する
+  return !prevPublickeyArticleLinks.includes(item.link);
+});
+const publickeyArticlesList = requiredPublickeyArticleList.reduce((prev: any, current: any, index: any) => {
   return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
 }, publickeyTitle);
+
+// 今回のPublickeyの記事リンクリストで上書き
+const currentPublickeyArticleLinks = requiredPublickeyArticleList.map((item: any) => {
+  return item.link;
+})
+// const data = JSON.stringify(currentPublickeyArticleLinks, null, 2); // 配列をJSON文字列に変換
+await fs.writeFile("tech-rss/publickey.json", JSON.stringify(currentPublickeyArticleLinks, null, 2), 'utf8');
+// await fs.writeFile("tech-rss/publickey.json", JSON.stringify(data, null, 2), 'utf8'); // ファイルに書き込む
 
 // Qiitaのトレンド記事を取得
 const qiitaTitle = '【<https://qiita.com/trend|Qiita>のトレンド記事】\n'
@@ -48,10 +66,11 @@ postData.append(
   `${articles}`
 );
 
-axios.post('https://slack.com/api/chat.postMessage', postData)
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// TODO: 一旦コメントアウト
+// axios.post('https://slack.com/api/chat.postMessage', postData)
+//   .then(response => {
+//     console.log('Response:', response.data);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
