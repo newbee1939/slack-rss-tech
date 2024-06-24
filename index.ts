7d2c59b0c4403d41608ecbb4dcbb89ebdd8aff87
newbee1939 <hideto3970@gmail.com>
2024-06-24 20:37:46 +0900

    a

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const PUBLICKEY_FILE_PATH = `${publickeyDirectoryPath}/publickey.json`;
 const zennTitle = '【<https://zenn.dev/|Zenn>のトレンド記事】\n'
 const zennArticles = await axios.get("https://zenn.dev/api/articles/");
 const zennArticlesList = zennArticles.data.articles.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<https://zenn.dev${current.path}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<https://zenn.dev${current.path}|${Bun.escapeHTML(current.title)}>\n` 
 }, zennTitle);
 
 // Publickeyの新着記事を取得
@@ -23,9 +23,9 @@ let prevPublickeyArticleLinks: any;
 try {
   prevPublickeyArticleLinks = await fs.readFile(PUBLICKEY_FILE_PATH, "utf8");
 } catch(error) {
-  // 対象のディレクトリが存在しない場合
+  // 対象のディレクトリが存在しない場合、作成する
   prevPublickeyArticleLinks = [];
-  await fs.ensureDir(directoryPath);
+  await fs.mkdir(publickeyDirectoryPath);
 }
 
 const requiredPublickeyArticleList = publickeyArticles.slice(0, ARTICLE_LIMIT).filter((item: any) => {
@@ -33,7 +33,7 @@ const requiredPublickeyArticleList = publickeyArticles.slice(0, ARTICLE_LIMIT).f
   return !prevPublickeyArticleLinks.includes(item.link);
 });
 const publickeyArticlesList = requiredPublickeyArticleList ? requiredPublickeyArticleList.reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<${current.link}|${Bun.escapeHTML(current.title)}>\n` 
 }, publickeyTitle) : publickeyTitle;
 
 // 今回のPublickeyの記事リンクリストで上書き
@@ -46,21 +46,21 @@ await fs.writeFile(PUBLICKEY_FILE_PATH, JSON.stringify(currentPublickeyArticleLi
 const qiitaTitle = '【<https://qiita.com/trend|Qiita>のトレンド記事】\n'
 const qiitaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://qiita.com/popular-items/feed') 
 const qiitaArticlesList = qiitaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<${current.link}|${Bun.escapeHTML(current.title)}>\n` 
 }, qiitaTitle);
 
 // はてなブックマークテクノロジーのトレンド記事を取得
 const hatenaTitle = '【<https://b.hatena.ne.jp/hotentry/it|はてなブックマーク(テクノロジー)>のトレンド記事】\n'
 const hatenaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://b.hatena.ne.jp/hotentry/it.rss') 
 const hatenaArticlesList = hatenaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<${current.link}|${Bun.escapeHTML(current.title)}>\n` 
 }, hatenaTitle);
 
 // ITmediaの新着記事を取得
 const itMediaTitle = '【<https://www.itmedia.co.jp/|ITmedia>の新着記事】\n'
 const itMediaArticles = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://rss.itmedia.co.jp/rss/2.0/topstory.xml') 
 const itMediaArticlesList = itMediaArticles.data.items.slice(0, ARTICLE_LIMIT).reduce((prev: any, current: any, index: any) => {
-  return prev + ' ' + `${index + 1}.<${current.link}|${current.title}>\n` 
+  return prev + ' ' + `${index + 1}.<${current.link}|${Bun.escapeHTML(current.title)}>\n` 
 }, itMediaTitle);
 
 const articles = ':robot_face:*本日の技術記事*' + "```" + `${hatenaArticlesList}\n${publickeyArticlesList}\n${zennArticlesList}\n${qiitaArticlesList}\n${itMediaArticlesList}` + "```";
